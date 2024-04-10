a4c0032d26e4d2e2c5cb7629ec83e94eb793032c
ARTEMZAHAROVAS <165668100+ARTEMZAHAROVAS@users.noreply.github.com>
2024-04-10 16:11:42 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+let audio = document.querySelector('.audio'),
+    buts = document.querySelector('.logo')
+
+buts.addEventListener('click' , e => {
+  buts.classList.toggle('paused')
+  if (audio.paused){
+    audio.play()   
+  }
+  else{
+    audio.pause()
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
